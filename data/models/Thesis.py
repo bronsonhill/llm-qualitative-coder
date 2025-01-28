2f8c135037bf5f9b97225f4000f66617f418ddc9
bronsonh <bronson.hill@student.unimelb.edu.au>
2025-01-28 11:22:06 +1100

    Introduce Thesis model getters for loading in json as df

--- data/models/Thesis.py
+++ data/models/Thesis.py
@@ -3,6 +3,7 @@ from sqlalchemy import (
 )
 from datetime import datetime, timezone
 from data.db import Base
+from sqlalchemy.orm import sessionmaker
 
 class Thesis(Base):
     __tablename__ = 'thesis'
@@ -23,6 +24,16 @@ class Thesis(Base):
     text = Column(Text, nullable=True)  # Full text of the thesis
     
     # Yahoo finance data
-    profile = Column(Text, nullable=True)  # Company profile data
-    monthly_performance = Column(JSON, nullable=True)  # List of date-performance pairs
+    profile = Column(JSON, nullable=True)  # Company profile data
+    daily_price = Column(JSON, nullable=True)  # List of date-performance pairs
     dividends = Column(JSON, nullable=True)  # List of date-dividend pairs
+
+    # def update_yahoo_finance_data(self):
+    #     prices_df, dividends_df, profile = get_yahoo_finance_data(self.ticker, self.date.strftime("%Y-%m-%d"))
+    #     self.market_cap = profile.get("marketCap")
+    #     self.profile = profile
+    #     # Convert prices_df to a list of date-performance pairs
+    #     self.monthly_performance = prices_df.reset_index().to_dict(orient='records')
+    #     # Convert dividends_df to a list of date-dividend pairs
+    #     self.dividends = dividends_df.reset_index().to_dict(orient='records')
+
